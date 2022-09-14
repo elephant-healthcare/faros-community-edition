d286254d4a35f89ad07be6a89f9203d25cbff12f
Brad Reed 😎 <brad@bradreed.co.uk>
2022-09-14 07:16:15 +0000

    tms_Team table

--- canonical-schema/V010__elephant_team_model.sql
+++ canonical-schema/V010__elephant_team_model.sql
@@ -7,3 +7,15 @@ create table "elephant_TeamRepositoryAssociation" (
 );
 alter table "elephant_TeamRepositoryAssociation" add foreign key (team) references "ims_Team"(id);
 alter table "elephant_TeamRepositoryAssociation" add foreign key (repository) references "vcs_Repository"(id);
+
+create table "tms_Team" (
+  id text generated always as (pkey(source, uid)) stored primary key,
+  origin text,
+  "refreshedAt" timestamptz not null default now(),
+  source text,
+  uid text not null,
+  name text not null
+);
+
+alter table "tms_Task" add column team text;
+alter table "tms_Task" add foreign key (team) references "tms_Team"(id);
